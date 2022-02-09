c4c038a006f03c90f0b34b6f49c1a24cf718af27
Ant Man <34754995+aaronamendez@users.noreply.github.com>
2022-02-09 17:45:00 -0500

    Setup Proj

--- api/server.js
+++ api/server.js
@@ -1,13 +1,17 @@
 const express = require('express');
-
+const router = require('./users/users-router');
+const middleware = require('./middleware/middleware');
 const server = express();
 
 // remember express by default cannot parse JSON in request bodies
-
+server.use(express.json());
+server.use(express.urlencoded({ extended: true }));
 // global middlewares and the user's router need to be connected here
 
+server.use('/api/users', router);
+
 server.get('/', (req, res) => {
-  res.send(`<h2>Let's write some middleware!</h2>`);
+	res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
 module.exports = server;
